a17820c1d1089396768f76963e8e2338239a564e
liangjun <liangjun@yuewen.com>
2021-01-21 20:03:34 +0800

    expire

--- redis/redis-6.0.6/src/t_string.c
+++ redis/redis-6.0.6/src/t_string.c
@@ -33,7 +33,7 @@
 /*-----------------------------------------------------------------------------
  * String Commands
  *----------------------------------------------------------------------------*/
-
+//检查字符大小是否大于512M
 static int checkStringLength(client *c, long long size) {
     if (size > 512*1024*1024) {
         addReplyError(c,"string exceeds maximum allowed size (512MB)");
